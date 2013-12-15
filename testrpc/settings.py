7fac0a4c7289f8ba582176a21f31411e1b87f8d8
Robert Kessler <sheepsy90@googlemail.com>
2013-12-15 19:08:54 +0100

    Fixed bug in path

--- testrpc/settings.py
+++ testrpc/settings.py
@@ -70,8 +70,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-     ("css",  "../dsoaxmlrpc/static/css/"),
-     ("js",  "../dsoaxmlrpc/static/js/"),
+     ("js",  "./static/js/"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
